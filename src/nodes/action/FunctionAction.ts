077e40c809440941f77de8924c912652f7cec14e
Josh Grant <jwaltgrant@gmail.com>
2020-06-20 19:34:28 -0500

    Function Action Tests

--- src/nodes/action/FunctionAction.ts
+++ src/nodes/action/FunctionAction.ts
@@ -12,47 +12,47 @@ export enum FunctionActionKey{
 }
 
 export class FunctionActions implements INodeActions<IFunctionAction> {
-         public instanceOf(node: IAbstractNode): boolean {
-           return "targets" in node;
-         }
-         public connectNode(connectionData: IConnect<IFunctionAction>) {
-           const connections = this.getConnectionList(
-             connectionData.fromNode,
-             connectionData.connectionKey
-           );
-           if (
-             connections &&
-             !connections.includes(connectionData.toNodeID)
-           ) {
-             connections.push(connectionData.toNodeID);
-           }
-           return { ...connectionData.fromNode };
-         }
+  public instanceOf(node: IAbstractNode): boolean {
+    return "targets" in node;
+  }
+  public connectNode(connectionData: IConnect<IFunctionAction>): IFunctionAction {
+    const connections = this.getConnectionList(
+      connectionData.fromNode,
+      connectionData.connectionKey
+    );
+    if (
+      connections &&
+      !connections.includes(connectionData.toNodeID)
+    ) {
+      connections.push(connectionData.toNodeID);
+    }
+    return { ...connectionData.fromNode };
+  }
 
-         public disconnectNode(connectionData: IConnect<IFunctionAction>) {
-           const connections = this.getConnectionList(
-             connectionData.fromNode,
-             connectionData.connectionKey
-           );
-           const index = connections.indexOf(connectionData.toNodeID);
-           if (index > -1) {
-             connections.splice(index, 1);
-           }
-           return { ...connectionData.fromNode };
-         }
+  public disconnectNode(connectionData: IConnect<IFunctionAction>): IFunctionAction {
+    const connections = this.getConnectionList(
+      connectionData.fromNode,
+      connectionData.connectionKey
+    );
+    const index = connections.indexOf(connectionData.toNodeID);
+    if (index > -1) {
+      connections.splice(index, 1);
+    }
+    return { ...connectionData.fromNode };
+  }
 
-         private getConnectionList(
-           node: IFunctionAction,
-           connectionKey: string
-         ): string[] {
-           switch (connectionKey) {
-             case FunctionActionKey.INPUT:
-               return node.parentNodeIDs;
-                case FunctionActionKey.OUTPUT:
-                    return node.targets;
-           }
-         }
-       }
+  private getConnectionList(
+    node: IFunctionAction,
+    connectionKey: string
+  ): string[] {
+    switch (connectionKey) {
+      case FunctionActionKey.INPUT:
+        return node.parentNodeIDs;
+        case FunctionActionKey.OUTPUT:
+            return node.targets;
+    }
+  }
+}
 
 export default class FunctionAction extends ActionNode implements IFunctionAction{
     actionKey: string = 'FUNCTION';
