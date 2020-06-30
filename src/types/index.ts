a2385240c7ab3cca4c9e278c06b3bc1691ffacc8
Josh Grant <jwaltgrant@gmail.com>
2020-06-29 19:45:39 -0500

    Start implementation custom types

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+import IFieldDef from "../FieldDef";
+import { IBlockImplementation } from "./BlockImplementation";
+
+export enum BaseTypes {
+  ANY = "any",
+  NUBMER = "number",
+  BOOLEAN = "boolean",
+  DATE = "date",
+  DATETIME = "datetime",
+  STRING = "string",
+}
+
+
+export interface IType {
+    typeName: string;
+    fields: IFieldDef[];
+    blocks: IBlockImplementation[];
+}
---
+++ src/types/redux/index.ts
@@ -0,0 +1,8 @@
+
+const initialState = {
+    
+}
+
+export default function typesStore(state = initialState, action: any){
+    return state;
+}
