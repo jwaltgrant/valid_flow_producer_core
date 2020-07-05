71b54b40980d3e37dc85f3b2844bef16f203b6dc
Josh Grant <jwaltgrant@gmail.com>
2020-07-04 20:25:38 -0500

    Add Args to IBlockImplementation

--- src/types/BlockImplementation.ts
+++ src/types/BlockImplementation.ts
@@ -1,9 +1,11 @@
 import { IInputNode } from "../nodes/io/InputNode";
 import { IOutputNode } from "../nodes/io/OutputNode";
 import { IActionNode } from "../nodes/action/ActionNode";
+import { IArgDef } from "../nodes/blockInstance/ArgDef";
 
 export interface IBlockImplementation {
   blockKey: string;
+  args: IArgDef[];
   inputNode: IInputNode;
   outputNodes: IOutputNode[];
   actions: IActionNode[];
