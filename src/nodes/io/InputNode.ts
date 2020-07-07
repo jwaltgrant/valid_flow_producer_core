7c4d06fce051b10db3efb831793ed2d09d517ef1
Josh Grant <jwaltgrant@gmail.com>
2020-07-06 21:47:48 -0500

    Add initializer

--- src/nodes/io/InputNode.ts
+++ src/nodes/io/InputNode.ts
@@ -1,5 +1,12 @@
-import { IAbstractNode } from "../AbstractNode";
+import { IAbstractNode, initAbstractNode } from "../AbstractNode";
 
 export interface IInputNode extends IAbstractNode {
-  targets: number[];
+  targets: string[];
+}
+
+export function initInputNode(id?: string): IInputNode{
+  return {
+    ...initAbstractNode(id),
+    targets: []
+  }
 }
